34c2475640d821e2f5c6b96849e5338c3579b54c
Marton Bodo <119077341+martonbodo@users.noreply.github.com>
2023-06-06 14:37:15 +0200

    Update and rename flocking.py to working_flocking.py
    fully working

--- working_flocking.py
+++ working_flocking.py
@@ -1,9 +1,10 @@
 from enum import Enum, auto
+
 import pygame as pg
 from pygame.math import Vector2
 from vi import Agent, Simulation
+from typing import Tuple
 from vi.config import Config, dataclass, deserialize
-import numpy as np
 
 
 @deserialize
@@ -17,7 +18,7 @@ class FlockingConfig(Config):
 
     mass: int = 20
 
-    def weights(self) -> tuple[float, float, float]:
+    def weights(self) -> Tuple[float, float, float]:
         return (self.alignment_weight, self.cohesion_weight, self.separation_weight)
 
 
@@ -28,32 +29,47 @@ class Bird(Agent):
         # Pac-man-style teleport to the other end of the screen when trying to escape
         self.there_is_no_escape()
         #YOUR CODE HERE -----------
-        # a, b, c = FlockingConfig().weights()
-        in_proximity = list(self.in_proximity_accuracy())
-        number_of_neighbours = self.in_proximity_accuracy().count()
-        if number_of_neighbours == 0:
+
+        if self.in_proximity_accuracy().count() == 0:
             self.pos += self.move
+    
         else:
-            velocity = Vector2(0,0)
-            pos = Vector2(0,0)
-            total_pos = Vector2(0,0)
-            for agent, dist in in_proximity:
-                velocity += agent.move.normalize()
-                pos += self.pos - agent.pos
-                total_pos += agent.pos
-            average_pos = total_pos / number_of_neighbours
-            fc = average_pos - self.pos
-            cohesion = fc - self.move
-            average_vel = velocity / number_of_neighbours
-            separtion = pos / number_of_neighbours
-            alignment = average_vel - self.move
-            acceleration = ((self.config.alignment_weight * alignment) + (self.config.separation_weight * separtion) + (self.config.cohesion_weight * cohesion)) / self.config.mass
+            # part 1 -- allignment
+            in_proximity = list(self.in_proximity_accuracy())
+            sum_velocity = Vector2(0,0)
+            for boid,distance in in_proximity:
+                sum_velocity += boid.move.normalize()
 
-            if self.move.length() > self.config.movement_speed:
-                self.move += (self.move.normalize() * self.move.length())
+            avg_velocity = sum_velocity/self.in_proximity_accuracy().count()
+            allignment = avg_velocity - self.move
 
-            self.move += acceleration 
+            # part 2 -- seperation
+            sum_pos = Vector2(0,0)
+            for boid,distance in in_proximity:
+                sum_pos += self.pos-boid.pos
+
+            seperation = sum_pos/self.in_proximity_accuracy().count()
+
+            # part 3 -- cohesion
+            avg_pos = Vector2(0,0)
+            for boid,distance in in_proximity:
+                avg_pos += boid.pos
+
+            avg_pos = avg_pos/self.in_proximity_accuracy().count()
+
+            cohesion_force = avg_pos-self.pos
+            cohesion = cohesion_force - self.move
+
+            total_force = (allignment*self.config.alignment_weight + seperation*self.config.separation_weight
+                            + cohesion*self.config.cohesion_weight) / self.config.mass
+
+
+            if self.move.length() > sum_velocity.length():
+                self.move.normalize() * sum_velocity
+
+            self.move+= total_force
             self.pos += self.move
+
 
         #END CODE -----------------
 
@@ -103,7 +119,6 @@ class FlockingLive(Simulation):
             movement_speed=1,
             radius=50,
             seed=1,
-            
         )
     )
     .batch_spawn_agents(50, Bird, images=["images/bird.png"])
