cc1a29a25671896d4263ebdb865b43dcf6df3d96
Elena-Andreea Bajenaru <andreeabajenaru2003@yahoo.com>
2023-06-13 11:30:04 +0200

    Update ass.py
    fixing git

--- Assignment_1/ass.py
+++ Assignment_1/ass.py
@@ -4,12 +4,14 @@ from pygame.math import Vector2
 from pygame.surface import Surface
 from vi import Agent, Simulation, Config
 import polars as pl
+from typing import list
+from typing import Union
 import seaborn as sns
 from vi.simulation import HeadlessSimulation
 
 
 class MyAgent(Agent):
-    def __init__(self, images: list[Surface], simulation: HeadlessSimulation, pos: Vector2 | None = None, move: Vector2 | None = None):
+    def __init__(self, images: list[Surface], simulation: HeadlessSimulation, pos: Union[Vector2, None] = None, move: Union[Vector2, None] = None):
         super().__init__(images, simulation, pos, move)
     
 """ def update(self):
@@ -21,6 +23,6 @@ config = Config()
 x, y = config.window.as_tuple()
 
 Simulation(Config(radius=50)).batch_spawn_agents(100, MyAgent,
-                    images=['images/red.png'
-                            ]).spawn_site("images/site.png", 
-                                          x//2, y//2).spawn_site("images/output-onlinepngtools.png", x=100, y=100).run()
+                    images=['/Users/malak/Desktop/Assignment_1/images/red.png'
+                            ]).spawn_site("/Users/malak/Desktop/Assignment_1/images/site.png", 
+                                          x//2, y//2).spawn_site("/Users/malak/Desktop/Assignment_1/images/output-onlinepngtools.png", x=100, y=100).run()
