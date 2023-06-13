312c3fe1a2d2d0b029b184eb49055a384f0f616d
John Grigoriadis <113774274+JohnGrigoriadis@users.noreply.github.com>
2023-06-13 12:50:15 +0200

    Update ass.py
    works

--- Assignment_1/ass.py
+++ Assignment_1/ass.py
@@ -4,25 +4,39 @@ from pygame.math import Vector2
 from pygame.surface import Surface
 from vi import Agent, Simulation, Config
 import polars as pl
-from typing import list
+from typing import List
 from typing import Union
-import seaborn as sns
+#import seaborn as sns
 from vi.simulation import HeadlessSimulation
 
 
 class MyAgent(Agent):
-    def __init__(self, images: list[Surface], simulation: HeadlessSimulation, pos: Union[Vector2, None] = None, move: Union[Vector2, None] = None):
+    def __init__(self, images: List[Surface], simulation: HeadlessSimulation, pos: Union[Vector2, None] = None, move: Union[Vector2, None] = None, count=20):
         super().__init__(images, simulation, pos, move)
-    
-""" def update(self):
+        self.count = count
+
+    def update(self):
         if self.on_site():
-        # crave that star damage
-            self.freeze_movement()"""
+            if self.count > 0:
+                self.count -= 1
+                self.pos += self.move
+            else:
+                self.freeze_movement()
+        else:
+            self.pos += self.move
+            #self.count = 10
+        print(self.count)
+        
+        
+        pass
 
 config = Config()
 x, y = config.window.as_tuple()
-
-Simulation(Config(radius=50)).batch_spawn_agents(100, MyAgent,
-                    images=["images/red.png"
-                            ]).spawn_site("images/site.png", 
-                                          x//2, y//2).spawn_site("images/output-onlinepngtools.png", x=100, y=100).run()
+(
+Simulation(Config(radius=25))
+                        .spawn_site("images/site.png", x//2, y//2)
+                        .batch_spawn_agents(50, MyAgent,images=["images/red.png"])
+                        #.spawn_site("images/site.png", x//2, y//2)
+                        
+                        .run()
+)
